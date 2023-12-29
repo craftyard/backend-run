750e5dc6662403d7707c3c4e866c03a27ad13965
Nuradil <yaksh0nti@gmail.com>
2023-12-30 00:22:55 +0500

    определен начальный конфиг репозитория

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ignorePatterns: ['.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    'max-classes-per-file': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+  },
+};
