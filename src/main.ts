96a992d0818e692a062818ded11a10e195178f94
Nuradil <yaksh0nti@gmail.com>
2024-01-03 13:15:37 +0500

    исправлен по код-ревью

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { InvalidJWTTokenExceptionFilter } from 'backend-core/src/infra/jwt/invalid-jwt-token-exception.filter';
 import { JSONWebTokenLibJWTManager } from 'backend-core/src/infra/jwt/jsonwebtoken-lib.jwt.manager';
-import { JWTConfig } from 'backend-core/src/config/jwt/types';
+import { jwtConfig } from 'backend-core/src/config/jwt/jwt-config';
 import { storeDispatcher } from 'rilata/src/app/async-store/store-dispatcher';
 import { JWTAuthGuard } from 'backend-core/src/infra/jwt/jwt-guard';
 import { AsyncLocalStorage } from 'async_hooks';
@@ -9,14 +9,6 @@ import { StorePayload } from 'rilata/src/app/async-store/types';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const jwtConfig: JWTConfig = {
-    algorithm: 'RS512',
-    privateKey: process.env.JWT_PRIVATE_KEY as string,
-    publicKey: process.env.JWT_PUBLIC_KEY as string,
-    accessTokenExpiresIn: '99d',
-    refreshTokenExpiresIn: '3d',
-  };
-
   const threadStore = new AsyncLocalStorage<StorePayload>();
   storeDispatcher.setThreadStore(threadStore);
 
@@ -24,6 +16,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule.forRoot(jwtManager));
   app.useGlobalFilters(new InvalidJWTTokenExceptionFilter());
   app.useGlobalGuards(new JWTAuthGuard(jwtManager));
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
