0263ca8ab0babbc9be54c62314d750dc729cfedd
Nuradil <yaksh0nti@gmail.com>
2024-01-18 14:20:55 +0500

    вместо пакета bun-types теперь @types/bun и обьявлен конфиг для pm2

---
+++ pm2-prod.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [{
+    name: 'craftyard-back',
+    script: './dist/src/main.js',
+    interpreter: '/Users/nuradilmaksut/.bun/bin/bun',
+    interpreter_args: 'run',
+  }],
+};
