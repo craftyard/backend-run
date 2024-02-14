93c75faaeb83949f5d408ba570ff45195ce3d983
Hetso <datacener23491248@gmail.com>
2024-02-14 12:57:57 +0500

    Пофиксил ошибки

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { RunMode } from 'rilata/src/app/types';
 import { ConsoleLogger } from 'rilata/src/common/logger/console-logger';
 import { JSONWebTokenLibJWTManager } from 'backend-core/src/infra/jwt/jsonwebtoken-lib.jwt.manager';
-import { SubjectReadNestModule } from 'cy-front-read/src/subject/nest-module';
-import { WorkshopReadNestModule } from 'cy-front-read/src/workshop/nest-module';
+import { SubjectWorkshopReadNestModule } from 'cy-front-read/src/subject-workshop/nest-module';
 
 @Module({})
 export class AppModule {
@@ -14,8 +13,7 @@ export class AppModule {
     return {
       module: AppModule,
       imports: [
-        SubjectReadNestModule.forRoot(consoleLogger, jwtManager, runMode),
-        WorkshopReadNestModule.forRoot(consoleLogger, jwtManager, runMode),
+        SubjectWorkshopReadNestModule.forRoot(consoleLogger, jwtManager, runMode),
       ],
     };
   }
